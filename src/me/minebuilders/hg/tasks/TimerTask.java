3a610f9386cd2a7e767811534e25aeffcb74f330
NemesisMate <nemesismatedev@gmail.com>
2013-09-23 10:36:48 +0200

    TimerTask.java, added "Game almost over" when few players remaining
    This is a fast implementation, not tested. However the players to consider "few" must be added to config.
    
    Another possibility if not by config is to get put "minPlayers / 2".
    
    (All config must be set by arena too instead of global)

--- src/me/minebuilders/hg/tasks/TimerTask.java
+++ src/me/minebuilders/hg/tasks/TimerTask.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 
 import me.minebuilders.hg.Game;
 import me.minebuilders.hg.HG;
+import me.minebuilders.hg.Status;
 
 public class TimerTask implements Runnable {
 
@@ -18,7 +19,8 @@ public class TimerTask implements Runnable {
 	public void run() {
 		remainingtime = (remainingtime - 30);
 
-		if (remainingtime == 30 && HG.plugin.getConfig().getBoolean("settings.teleport-at-end")) {
+		// 4 must be add to config arena config
+		if (((g.getStatus() != Status.RUNNING && g.getPlayers().size() < 4) || remainingtime == 30) && HG.plugin.getConfig().getBoolean("settings.teleport-at-end")) {
 			game.msgAll("&l&cThe game is almost over, fight to the death!");
 			game.respawnAll();
 		} else if (this.remainingtime < 10) {
